6ca3a6c434af8059d3cb98b608b0c1e8df61fe9f
vynijales <vynijales@gmail.com>
2025-10-16 16:11:55 -0300

    fix: criar expressões regulares para identificadores e adicionar meta atributos

--- main.py
+++ main.py
@@ -77,7 +77,6 @@ KEYWORDS = [
     "functional-complexes",
 ]
 
-
 SYMBOLS = [
     "{",
     "}",
@@ -93,23 +92,31 @@ SYMBOLS = [
     ":"
 ]
 
+META_ATTRIBUTES = [
+    "ordered",
+    "const",
+    "derived",
+    "subsets",
+    "redefines"
+]
+
 """
 Convenção para nomes de classes: iniciando com letra maiúscula, seguida por qualquer
 combinação de letras, ou tendo sublinhado como subcadeia própria, sem números. Exemplos:
 Person, Child, Church, University, Second_Baptist_Church.
 """
-t_CLASS_IDENTIFIER = r'[A-Z][a-z]+(_[A-Z][a-z])*'
+t_CLASS_IDENTIFIER = r'[A-Z][a-z]+(_[A-Z][a-z]+)*'
 
 """
 Convenção para nomes de relações: começando com letra minúscula, seguida por qualquer
 combinação de letras, ou tendo sublinhado como subcadeia própria, sem números. Exemplos:
 has, hasParent, has_parent, isPartOf, is_part_of.
 """
-t_RELATION_IDENTIFIER = r''
+t_RELATION_IDENTIFIER = r'[a-z][a-z]*(_[a-z][a-z]*)*'
 
 """
 Convenção para nomes de instâncias: iniciando com qualquer letra, podendo ter o sublinhado
 como subcadeia própria e terminando com algum número inteiro. Exemplos: Planeta1, Planeta2,
 pizza03, pizza123
 """
-t_INSTANCE_IDENTIFIER = r''
+t_INSTANCE_IDENTIFIER = r'[A-Za-z][A-Za-z]*(_[A-Za-z][A-Za-z]*)*\d+'
