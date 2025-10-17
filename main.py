6e0df4ce016f70d6f746cfe7d5ad8d4b10aec006
Thiago Coelho <thiago.coelho158@gmail.com>
2025-10-17 18:37:03 -0300

    feat: adicionar restante das regras ao lexer
    Foram adicionadas as regras de identificação dos seguintes tokens:
    
    - KEYWORD
    - SYMBOL
    - CLASS_STEREOTYPE
    - RELATION_STEREOTYPE
    - META_ATTRIBUTES
    - NATIVE_TYPE
    - NUMBER
    - COMMA
    - newline

--- main.py
+++ main.py
@@ -13,6 +13,9 @@ tokens = (
     'NATIVE_TYPE',
     'USER_TYPE',
     'META_ATTRIBUTES',
+
+    'NUMBER',
+    'COMMA',
 )
 
 CLASS_STEREOTYPES = [
@@ -100,6 +103,15 @@ META_ATTRIBUTES = [
     "redefines"
 ]
 
+NATIVE_TYPES = [
+    "number",
+    "string",
+    "boolean",
+    "date",
+    "time",
+    "datetime",
+]
+
 """
 Convenção para nomes de classes: iniciando com letra maiúscula, seguida por qualquer
 combinação de letras, ou tendo sublinhado como subcadeia própria, sem números. Exemplos:
@@ -119,4 +131,32 @@ Convenção para nomes de instâncias: iniciando com qualquer letra, podendo ter
 como subcadeia própria e terminando com algum número inteiro. Exemplos: Planeta1, Planeta2,
 pizza03, pizza123
 """
-t_INSTANCE_IDENTIFIER = r'[A-Za-z][A-Za-z]*(_[A-Za-z][A-Za-z]*)*\d+'
+t_INSTANCE_IDENTIFIER = r'[a-zA-Z][a-zA-Z_]*\d+'
+
+
+t_KEYWORD = r'|'.join(KEYWORDS)
+t_SYMBOL = r'|'.join([r'\{}'.format(sym) for sym in SYMBOLS])
+t_CLASS_STEREOTYPE = r'|'.join(CLASS_STEREOTYPES)
+t_RELATION_STEREOTYPE = r'|'.join(RELATION_STEREOTYPES)
+t_META_ATTRIBUTES = r'|'.join(META_ATTRIBUTES)
+t_NATIVE_TYPE = r'|'.join(NATIVE_TYPES)
+
+t_NUMBER = r'\d+'
+t_COMMA = r','
+
+# Define a rule so we can track line numbers
+def t_newline(t):
+    r'\n+'
+    t.lexer.lineno += len(t.value)
+
+# A string containing ignored characters (spaces and tabs)
+t_ignore  = ' \t'
+
+# Error handling rule
+def t_error(t):
+    print("Illegal character '%s'" % t.value[0])
+    t.lexer.skip(1)
+
+
+# Build the lexer
+lexer = lex.lex()
