e4ccff567a4e5dd88a84b02cf7a874670c856655
Thiago Coelho <thiago.coelho158@gmail.com>
2025-10-20 01:18:54 -0300

    feat(LEXER): Ajustar syntax highlight

--- src/ui/widgets/syntax_highlighter.py
+++ src/ui/widgets/syntax_highlighter.py
@@ -1,25 +1,32 @@
 from PyQt5.QtGui import QTextCharFormat, QColor, QSyntaxHighlighter
-from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
-
+from lexer.lexer import SYMBOLS
 
 class SyntaxHighlighter(QSyntaxHighlighter):
     def __init__(self, parent=None):
         super().__init__(parent)
+        
         self.token_colors = {
-            'KEYWORD': QColor('blue'),
-            'CLASS_IDENTIFIER': QColor('#ff0000'),
-            'INSTANCE_IDENTIFIER': QColor('#ff8500'),
-            'RELATION_IDENTIFIER': QColor('#ffcb00'),
-            'CLASS_STEREOTYPE': QColor('#800080'),
-            'RELATION_STEREOTYPE': QColor('#ff1493'),
-            'META_ATTRIBUTES': QColor('#008080'),
-            "SYMBOL": QColor('#a52a2a'),
-            'NUMBER': QColor('#ff4500'),
-            'NATIVE_TYPE': QColor('#00f050'),
-            'USER_TYPE': QColor('#0050f0'),
-            'COMMA': QColor('#000000'),
-            'COMMENT': QColor('#444444'),
+            'KEYWORD': QColor('#F92672'),               # Magenta
+            'CLASS_IDENTIFIER': QColor('#A6E22E'),      # Verde Claro
+            'INSTANCE_IDENTIFIER': QColor('#FD971F'),   # Laranja
+            'RELATION_IDENTIFIER': QColor('#E6DB74'),   # Amarelo Claro
+            'CLASS_STEREOTYPE': QColor('#AE81FF'),      # Roxo/Lilás
+            'RELATION_STEREOTYPE': QColor('#F92672'),   # Magenta
+            'META_ATTRIBUTES': QColor('#66D9EF'),       # Ciano
+        
+            'NUMBER': QColor('#AE81FF'),                # Roxo/Lilás
+            'NATIVE_TYPE': QColor('#A6E22E'),           # Verde Claro
+            'USER_TYPE': QColor('#66D9EF'),             # Ciano
+            
+            # Quase Branco (Texto Principal)
+            'COMMA': QColor('#F8F8F2'),
+            'COMMENT': QColor('#75715E'),               # Cinza Esverdeado
+            'ERROR': QColor('#FF2050'),                 # Vermelho Forte
         }
+
+        for s in SYMBOLS.keys():
+            self.token_colors[s] = QColor('#00FFFF')
+        
         self.highlighting_rules = []
 
     def set_tokens(self, tokens):
@@ -29,6 +36,14 @@ class SyntaxHighlighter(QSyntaxHighlighter):
             if token.type in self.token_colors:
                 format = QTextCharFormat()
                 format.setForeground(self.token_colors[token.type])
+                self.highlighting_rules.append((token, format))
+
+            if token.type == 'ERROR':
+                format = QTextCharFormat()
+                format.setForeground(self.token_colors['ERROR'])
+                format.setUnderlineColor(QColor(self.token_colors['ERROR']))
+                format.setUnderlineStyle(QTextCharFormat.SpellCheckUnderline)
+                format.setFontWeight(100)
                 self.highlighting_rules.append((token, format))
 
     def highlightBlock(self, text):
