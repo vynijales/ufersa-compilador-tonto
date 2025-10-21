b11a25068109059fcf73270f85422836d9219223
vynijales <vynijales@gmail.com>
2025-10-20 23:10:13 -0300

    fix(UI): adicionar estilo para QDialog e remover ChartWidget

--- src/ui/widgets/__init__.py
+++ src/ui/widgets/__init__.py
@@ -1,4 +1,3 @@
-from .chart_widget import ChartWidget
 from .closeable_tab import CloseableTabWidget
 from .code_editor import CodeEditor
 from .file_tab import FileTab
--- src/ui/widgets/chart_widget.py
+++
@@ -1,9 +0,0 @@
-from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
-from matplotlib.figure import Figure
-
-class ChartWidget(FigureCanvas):
-    def __init__(self):
-        self.fig = Figure(figsize=(6, 4), dpi=80)
-        super().__init__(self.fig)
-        self.ax = self.fig.add_subplot(111)
-
