9c02faa50e041cb26301fc1704e509d17642513c
vynijales <vynijales@gmail.com>
2025-11-21 19:55:56 -0300

    feat(teste): adicionar teste para o parser com exemplos de datatypes e kind

---
+++ src/teste.py
@@ -0,0 +1,62 @@
+from parser.parser import parser
+
+teste1 = """
+package teste
+
+datatype AddressDataType {
+    street: string
+    city: number
+}
+
+kind Person {
+    name: string
+    age: number
+    address: AddressDataType
+}
+
+datatype CompanyDataType {
+    name: string
+}
+
+"""
+
+def test_parser():
+    result = parser.parse(teste1)
+
+    assert result == {
+    'package_name': 'teste',
+    'imports': [],
+    'declarations': [
+        {
+            'type': 'datatype',
+            'name': 'AddressDataType',
+            'attributes': [
+                {'name': 'street', 'type': 'string'},
+                {'name': 'city', 'type': 'number'},
+            ],
+        },
+        {
+            'type': 'kind',
+            'name': 'Person',
+            'content': {
+                'atributes': [
+                    {'name': 'name', 'type': 'string'},
+                    {'name': 'age', 'type': 'number'},
+                    {'name': 'address', 'type': 'AddressDataType'},
+                ],
+                'relations': [],
+            },
+        },
+        {
+            'type': 'datatype',
+            'name': 'CompanyDataType',
+            'attributes': [
+                {'name': 'name', 'type': 'string'},
+            ],
+        },
+    ],
+}
+
+if __name__ == "__main__":
+    test_parser()
+    print("All tests passed.")
