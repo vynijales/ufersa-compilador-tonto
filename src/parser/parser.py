da08795dadfe57708f60c918598ee93ea075c4ee
Thiago Coelho <thiago.coelho158@gmail.com>
2025-11-24 23:13:59 -0300

    docs(parser): adicionar comentários explicativos nas funções do parser

--- src/parser/parser.py
+++ src/parser/parser.py
@@ -1,9 +1,9 @@
 from ply import yacc
 from lexer.lexer import tokens, TontoLexer
-import json
 
 # =========== Ontology ===========
 
+# Regra principal que define a estrutura de uma ontologia
 def p_ontology(p):
     '''ontology : package imports declarations'''
     p[0] = {
@@ -13,6 +13,7 @@ def p_ontology(p):
     }
 
 
+# Lista de declarações (classes, enums, gensets, etc.)
 def p_declarations(p):
     '''
     declarations : declarations declaration
@@ -24,15 +25,18 @@ def p_declarations(p):
         p[0] = []
 
 
+# Define os tipos de declaração válidos na linguagem
 def p_declaration(p):
     '''declaration : datatype_declaration
                    | class_declaration
                    | enum_declaration
                    | genset_declaration
+                   | relation_external
     '''
     p[0] = p[1]
 
 
+# Declaração de tipo de dados personalizado
 def p_datatype_declarion(p):
     '''datatype_declaration : DATATYPE_KW USER_TYPE OPEN_BRACE attr_list CLOSE_BRACE'''
     p[0] = {
@@ -42,6 +46,7 @@ def p_datatype_declarion(p):
     }
 
 
+# Tipos de dados (nativos ou definidos pelo usuário)
 def p_datatype(p):
     '''datatype : NATIVE_TYPE
                 | USER_TYPE
@@ -49,6 +54,7 @@ def p_datatype(p):
     p[0] = p[1]
 
 
+# Lista de imports de outros pacotes
 def p_imports(p):
     '''
     imports : imports import
@@ -60,17 +66,20 @@ def p_imports(p):
         p[0] = []
 
 
+# Declaração de import de um pacote
 def p_import(p):
     '''import : IMPORT_KW IDENTIFIER'''
     p[0] = p[2]
 
 
+# Declaração do nome do pacote
 def p_package(p):
     '''package : PACKAGE_KW IDENTIFIER'''
     p[0] = p[2]
 
 # ============== Class ==============
 
+# Declaração de classe
 def p_class_declaration(p):
     '''class_declaration : CLASS_STEREOTYPE IDENTIFIER optional_class_body'''
     p[0] = {
@@ -80,6 +89,7 @@ def p_class_declaration(p):
     }
 
 
+# Declaração de classe com herança/especialização
 def p_class_specialization(p):
     '''class_declaration : CLASS_STEREOTYPE IDENTIFIER SPECIALIZES_KW IDENTIFIER optional_class_body'''
     p[0] = {
@@ -90,6 +100,7 @@ def p_class_specialization(p):
     }
 
 
+# Corpo opcional da classe com atributos e relações
 def p_optional_class_body(p):
     '''optional_class_body : OPEN_BRACE attr_list relation_list CLOSE_BRACE
                            | empty
@@ -106,6 +117,7 @@ def p_optional_class_body(p):
         }
 
 
+# Lista de atributos de uma classe
 def p_attr_list(p):
     '''
     attr_list : attr_list attribute
@@ -117,6 +129,7 @@ def p_attr_list(p):
         p[0] = []
 
 
+# Declaração de atributo em uma classe com tipo e metadados opcionais
 def p_attribute(p):
     '''
     attribute : IDENTIFIER COLON datatype
@@ -130,14 +143,16 @@ def p_attribute(p):
     }
 
 
+# Metaatributos como ordered, const, derived, etc.
 def p_meta_attributes(p):
     '''meta_attributes : OPEN_BRACE META_ATTRIBUTES CLOSE_BRACE'''
     p[0] = p[2]
 
 
+# Lista de relações internas de uma classe
 def p_relation_list(p):
     '''
-    relation_list : relation_list relation
+    relation_list : relation_list relation_internal
                   | empty
     '''
     if len(p) == 3:
@@ -155,6 +170,7 @@ def p_relation(p):
     }
 
 
+# Define cardinalidade de atributos e relações
 def p_cardinality(p):
     '''cardinality : OPEN_BRACKET MULTIPLICATION CLOSE_BRACKET
                    | OPEN_BRACKET NUMBER CLOSE_BRACKET
@@ -166,6 +182,7 @@ def p_cardinality(p):
 
 # =============== Enum ===============
 
+# Declaração de enumeração
 def p_enum_declaration(p):
     '''enum_declaration : ENUM_KW IDENTIFIER OPEN_BRACE enum_values CLOSE_BRACE'''
     p[0] = {
@@ -175,11 +192,13 @@ def p_enum_declaration(p):
     }
 
 
+# Valor individual de uma enumeração
 def p_enum_value(p):
     '''enum_value : IDENTIFIER'''
     p[0] = p[1]
 
 
+# Lista de valores de enumeração separados por vírgula
 def p_enum_values(p):
     '''
     enum_values : enum_values COMMA enum_value
@@ -192,6 +211,8 @@ def p_enum_values(p):
 
 # ============== Genset ==============
 
+
+# Declaração de genset (conjunto de generalização)
 def p_genset_declaration(p):
     '''genset_declaration : GENSET_KW IDENTIFIER OPEN_BRACE GENERAL_KW IDENTIFIER SPECIFICS_KW comma_separated_identifiers CLOSE_BRACE'''
     p[0] = {
@@ -202,6 +223,8 @@ def p_genset_declaration(p):
         'general': p[5],
     }
 
+
+# Declaração de genset na forma inline com restrições
 def p_genset_declaration_inline(p):
     '''genset_declaration : optional_genset_restrictions GENSET_KW IDENTIFIER WHERE_KW comma_separated_identifiers SPECIALIZES_KW IDENTIFIER'''
     p[0] = {
@@ -212,6 +235,8 @@ def p_genset_declaration_inline(p):
         'general': p[7],
     }
 
+
+# Lista opcional de restrições de genset (disjoint, complete, etc.)
 def p_optional_genset_restrictions(p):
     '''
     optional_genset_restrictions : optional_genset_restrictions genset_restriction
@@ -222,6 +247,8 @@ def p_optional_genset_restrictions(p):
     else:
         p[0] = []
 
+
+# Restrições aplicáveis a gensets
 def p_genset_restriction(p):
     '''
     genset_restriction : DISJOINT_KW
@@ -230,6 +257,7 @@ def p_genset_restriction(p):
                        | OVERLAPPING_KW
     '''
     p[0] = p[1]
+
 
 # Descrição: Essa regra define uma produção em que deve haver ao menos um
 # identificador. Se houver mais que um, devem estar separados por vírgula
@@ -252,6 +280,7 @@ def p_empty(p):
     pass
 
 
+# Trata erros sintáticos durante o parsing
 def p_error(p):
     if p:
         print(f"Syntax error at {p.value!r} in line {p.lineno}")
