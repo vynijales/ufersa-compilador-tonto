44610a0f9e58de1c4916bcf2f409b31ea491a8c5
Thiago Coelho <thiago.coelho158@gmail.com>
2025-12-15 23:42:26 -0300

    refactor(teste.py): remove hardcoded test data and read from file

--- src/teste.py
+++ src/teste.py
@@ -1,53 +1,10 @@
 from parser.parser import parse_ontology
 from semantic.analyzer import analyze, print_analysis_results
 
-teste1 = """
-package teste
-
-enum Color {
-    RED,
-    GREEN,
-    BLUE
-}
-
-enum Size { SMALL }
-
-subkind Child specializes Person
-kind Adult
-
-datatype AddressDataType {
-    street: string
-    city: number
-}
-
-kind Person {
-    name: string
-    age: number
-    address: AddressDataType
-}
-
-datatype CompanyDataType {
-    name: string
-}
-
-//disjoint complete genset PersonAgeGroup where Child, Adult specializes Person
-//
-//genset PersonAgeGroup {
-//    general Person
-//    specifics Child, Adult
-//}
-
-
-kind House {
-    color: string
-}
-
-@componentOf
-relation Person [0..*] <>-- possui -- [0..*] House
-"""
-
 
 if __name__ == "__main__":
+    with open("exemplos/unidade-3/Hospital_Mono.tonto", "r") as f:
+        teste1 = f.read()
     ast = parse_ontology(teste1)
     st, err = analyze(ast)
     print_analysis_results(st, err)
